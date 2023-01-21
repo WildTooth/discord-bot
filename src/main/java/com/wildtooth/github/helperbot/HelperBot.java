4dc205964d725cea081be1eed6a85bf039360240
WildTooth <champenV@outlook.dk>
2023-01-22 00:02:01 +0100

    Added getInstance() method

--- src/main/java/com/wildtooth/github/helperbot/HelperBot.java
+++ src/main/java/com/wildtooth/github/helperbot/HelperBot.java
@@ -4,14 +4,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HelperBot extends JavaPlugin {
 
+    private static HelperBot instance;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+        instance = this;
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static HelperBot getInstance() {
+        return instance;
     }
 }
